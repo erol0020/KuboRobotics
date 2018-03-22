b847d365183d0e9f03309ab88f052fd65f259b05
Erol Bakic <erol.bakic@hotmail.com>
2018-03-22 13:23:48 +0100

    Tilføjede header

--- index.php
+++ index.php
@@ -12,8 +12,31 @@
   <!-- CSS -->
   <link rel="stylesheet" href="css/main.css">
 
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/png" href="img/icon.ico">
+
 </head>
-<body>
-  
+<body class="main-wrapper">
+  <header class="main-header">
+    <a class="main-logo" href="./index.php">
+      <img src="img/logo.png" alt="Kedu logo">
+    </a><!--/.main-logo-->
+
+
+    <nav class="main-nav">
+      <ul>
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Lesson Plans</a></li>
+        <li><a href="?page=Documentation">Documentation</a></li>
+        <li><a href="?page=Educational-Materials">Educational Materials</a></li>
+        <li><a href="?page=Forum">Forum</a></li>
+      </ul>
+    </nav><!--/.main-nav-->
+
+    <a href="#" class="main-profile">
+      <span class="profile-icon"></span>Flemming Dibs
+    </a>
+  </header><!--/.main-header-->
+
 </body>
 </html>
