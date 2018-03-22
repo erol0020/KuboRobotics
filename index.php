be816a455350341804a2ffd6f8fc136935bbbce4
Erol Bakic <erol.bakic@hotmail.com>
2018-03-22 11:57:13 +0100

    Added base HTML and CSS

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Title -->
+  <title>KEDU - The Educational Community and Curriculum Platform</title>
+
+  <!-- Meta -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/main.css">
+
+</head>
+<body>
+  
+</body>
+</html>
